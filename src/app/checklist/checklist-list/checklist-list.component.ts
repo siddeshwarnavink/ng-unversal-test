8eda8a18a2dd19a07c528700bad312c2a19fdc01
Siddeshwar <siddeshwar.work@gmail.com>
2023-05-14 11:07:45 +0530

    created checklist list component

---
+++ src/app/checklist/checklist-list/checklist-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checklist-list',
+  templateUrl: './checklist-list.component.html',
+  styleUrls: ['./checklist-list.component.css']
+})
+export class ChecklistListComponent {
+
+}
--- src/app/checklist/checklist.module.ts
+++ src/app/checklist/checklist.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddChecklistItemComponent } from './add-checklist-item/add-checklist-item.component';
+import { ChecklistListComponent } from './checklist-list/checklist-list.component';
 
 
 
 @NgModule({
   declarations: [
-    AddChecklistItemComponent
+    AddChecklistItemComponent,
+    ChecklistListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    AddChecklistItemComponent
+    AddChecklistItemComponent,
+    ChecklistListComponent
   ]
 })
 export class ChecklistModule { }
