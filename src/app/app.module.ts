0c7627f45d62d4e35ec5e28a437244406a0d6980
Siddeshwar <siddeshwar.work@gmail.com>
2023-05-14 12:04:34 +0530

    enabled SSR using angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +16,9 @@ import { ChecklistModule } from './checklist/checklist.module';
     HttpClientModule,
     ChecklistModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
